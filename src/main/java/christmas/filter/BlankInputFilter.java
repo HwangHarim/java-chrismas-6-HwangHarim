26a9df16a28092978d61616d0576e5a633024073
HwangHarim <scopan01@naver.com>
2023-11-15 22:42:42 +0900

    feat : 공백 예외처리

---
+++ src/main/java/christmas/filter/BlankInputFilter.java
@@ -0,0 +1,23 @@
+package christmas.filter;
+
+import christmas.error.ErrorMessage;
+import christmas.error.exception.OrderMenuInputException;
+import christmas.error.exception.ReservationDayInputException;
+import java.util.Objects;
+
+public class BlankInputFilter {
+
+    public String blankInputReservationDayFilter(String input){
+        if(Objects.equals(input, "")){
+            throw new ReservationDayInputException(ErrorMessage.INPUT_RESERVATION_DAY_EXCEPTION);
+        }
+        return input;
+    }
+
+    public String blankInputOrderMenuFilter(String input){
+        if(Objects.equals(input, "")){
+            throw new OrderMenuInputException(ErrorMessage.INPUT_ORDER_MENU_EXCEPTION);
+        }
+        return input;
+    }
+}
